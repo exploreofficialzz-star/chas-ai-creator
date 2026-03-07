9f7644bc4408a9267ddaf264d6247b6815ecd10d
chAs <exploreofficialzz@gmail.com>
2026-03-07 14:59:21 -0500

    Create custom_text_field.dart

---
+++ frontend/lib/widgets/custom_text_field.dart
@@ -0,0 +1,78 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
+
+import '../config/theme.dart';
+
+class CustomTextField extends StatelessWidget {
+  final TextEditingController controller;
+  final String label;
+  final String? hint;
+  final bool obscureText;
+  final TextInputType keyboardType;
+  final IconData? prefixIcon;
+  final IconData? suffixIcon;
+  final VoidCallback? onSuffixTap;
+  final String? Function(String?)? validator;
+  final int? maxLines;
+  final int? maxLength;
+  final bool readOnly;
+  final VoidCallback? onTap;
+  final Function(String)? onChanged;
+
+  const CustomTextField({
+    super.key,
+    required this.controller,
+    required this.label,
+    this.hint,
+    this.obscureText = false,
+    this.keyboardType = TextInputType.text,
+    this.prefixIcon,
+    this.suffixIcon,
+    this.onSuffixTap,
+    this.validator,
+    this.maxLines = 1,
+    this.maxLength,
+    this.readOnly = false,
+    this.onTap,
+    this.onChanged,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      crossAxisAlignment: CrossAxisAlignment.start,
+      children: [
+        Text(
+          label,
+          style: Theme.of(context).textTheme.bodyMedium?.copyWith(
+            fontWeight: FontWeight.w500,
+          ),
+        ),
+        SizedBox(height: 8.h),
+        TextFormField(
+          controller: controller,
+          obscureText: obscureText,
+          keyboardType: keyboardType,
+          validator: validator,
+          maxLines: maxLines,
+          maxLength: maxLength,
+          readOnly: readOnly,
+          onTap: onTap,
+          onChanged: onChanged,
+          decoration: InputDecoration(
+            hintText: hint,
+            prefixIcon: prefixIcon != null
+                ? Icon(prefixIcon, color: AppTheme.textSecondaryLight)
+                : null,
+            suffixIcon: suffixIcon != null
+                ? GestureDetector(
+                    onTap: onSuffixTap,
+                    child: Icon(suffixIcon, color: AppTheme.textSecondaryLight),
+                  )
+                : null,
+          ),
+        ),
+      ],
+    );
+  }
+}
