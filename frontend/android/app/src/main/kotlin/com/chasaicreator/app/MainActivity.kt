3d8b51f8b16eabefc4528a9af8a4398410d2c856
chAs <exploreofficialzz@gmail.com>
2026-03-09 12:42:26 -0400

    Update MainActivity.kt

--- frontend/android/app/src/main/kotlin/com/chasaicreator/app/MainActivity.kt
+++ frontend/android/app/src/main/kotlin/com/chasaicreator/app/MainActivity.kt
@@ -1,8 +1,5 @@
 package com.chasaicreator.app
 
-import io.flutter.embedding.android.FlutterActivity
 import io.flutter.embedding.android.FlutterFragmentActivity
 
-class MainActivity: FlutterFragmentActivity() {
-}
-
+class MainActivity: FlutterFragmentActivity()
